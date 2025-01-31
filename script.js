f43f2d915dc3f7038b7551342ca6e36eb3bf6aa2
U V Muralidhar Thunuguntla <53929861+Upendra-Thunuguntla@users.noreply.github.com>
2025-01-31 09:50:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ function postQuery() {
             // .then(result => message.value="")
             .catch(error => console.log('error', error))
             .then(() => {
-                window.open("https://t.me/infosys_queries", "_self");
+                window.open("https://t.me/infy_queries", "_self");
                 // window.open("index.html", "_self")
             });
     }
